f7e4b5c1c45a48d86cc5b52d32e512a55f65b830
Luiz Felipe <luiz.cdurante123@gmail.com>
2023-10-03 12:06:25 -0300

    add: vavr para programacao funcional

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -11,9 +11,12 @@ repositories {
 
 dependencies {
     implementation(project(":domain"))
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation ("org.mockito:mockito-junit-jupiter:5.4.0")
+    implementation("io.vavr:vavr:0.10.4")
+
 }
 
 tasks.test {
