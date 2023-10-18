80f0f42239d4408fe6e8209e5876d7bcc7486aaa
Luiz Felipe <luiz.cdurante123@gmail.com>
2023-10-18 11:22:12 -0300

    add: use case get category

---
+++ application/src/main/java/com/felipe/admin/catalogo/application/category/find/get/DefaultGetCategoryUseCase.java
@@ -0,0 +1,31 @@
+package com.felipe.admin.catalogo.application.category.find.get;
+
+import com.felipe.admin.catalogo.domain.category.CategoryGateway;
+import com.felipe.admin.catalogo.domain.category.CategoryID;
+import com.felipe.admin.catalogo.domain.exceptions.DomainException;
+import com.felipe.admin.catalogo.domain.validation.Error;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class DefaultGetCategoryUseCase extends GetCategoryUseCase {
+
+    private final CategoryGateway gateway;
+
+    public DefaultGetCategoryUseCase(CategoryGateway gateway) {
+        this.gateway = Objects.requireNonNull(gateway);
+    }
+
+    @Override
+    public CategoryOutPut execute(final String anIn) {
+        final var anCategoryID = CategoryID.from(anIn);
+
+        return this.gateway.findById(anCategoryID)
+                .map(CategoryOutPut::from)
+                .orElseThrow(notFound(anCategoryID));
+    }
+
+    private Supplier<DomainException> notFound(final CategoryID anId) {
+        return () -> DomainException.with(new Error("Category with ID %s was not found".formatted(anId.getValue())));
+    }
+}
---
+++ application/src/main/java/com/felipe/admin/catalogo/application/category/find/get/GetCategoryUseCase.java
@@ -0,0 +1,8 @@
+package com.felipe.admin.catalogo.application.category.find.get;
+
+import com.felipe.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryUseCase extends UseCase<String, CategoryOutPut> {
+
+
+}
