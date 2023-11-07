732ddc4a76b53cd2da39f68057f75e7f0a3ff747
Luiz Felipe <luiz.cdurante123@gmail.com>
2023-11-07 14:22:34 -0300

    feat(categoryAPI): adicionado controller

---
+++ infrastructure/src/main/java/com/felipe/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.felipe.admin.catalogo.infrastructure.api.controllers;
+
+import com.felipe.admin.catalogo.domain.pagination.Pagination;
+import com.felipe.admin.catalogo.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String dir) {
+        return null;
+    }
+}
