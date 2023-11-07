16c10e4678bb1a09e6042fbb426431db54ff2c04
Luiz Felipe <luiz.cdurante123@gmail.com>
2023-11-07 14:23:42 -0300

    feat(categoryUseCase)

--- infrastructure/src/main/java/com/felipe/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/felipe/admin/catalogo/infrastructure/Main.java
@@ -1,14 +1,33 @@
 package com.felipe.admin.catalogo.infrastructure;
 
+import com.felipe.admin.catalogo.domain.category.Category;
+import com.felipe.admin.catalogo.infrastructure.category.persistence.CategoryEntity;
+import com.felipe.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.felipe.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,"development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
+
+//    @Bean
+//    public ApplicationRunner runner(CategoryRepository categoryRepository) {
+//        return args -> {
+//            var all = categoryRepository.findAll();
+//
+//            var filmes = Category.newCategory("Filmes", "descricao", true);
+//
+//            categoryRepository.saveAndFlush(CategoryEntity.from(filmes));
+//
+//            categoryRepository.deleteAll();
+//
+//        };
+//    }
 }
