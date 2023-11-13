078843e6eea27643a2f183de8b5d87342afe3911
Luiz Felipe <luiz.cdurante123@gmail.com>
2023-11-13 10:34:08 -0300

    feat(implementation):listCategory

--- infrastructure/src/main/java/com/felipe/admin/catalogo/infrastructure/category/CategoryMySqlGateway.java
+++ infrastructure/src/main/java/com/felipe/admin/catalogo/infrastructure/category/CategoryMySqlGateway.java
@@ -8,10 +8,16 @@ import com.felipe.admin.catalogo.domain.pagination.Pagination;
 import com.felipe.admin.catalogo.domain.pagination.SearchQuery;
 import com.felipe.admin.catalogo.infrastructure.category.persistence.CategoryEntity;
 import com.felipe.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import com.felipe.admin.catalogo.infrastructure.utils.SpecificationUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
+
 
 @Service
 public class CategoryMySqlGateway implements CategoryGateway {
@@ -48,17 +54,49 @@ public class CategoryMySqlGateway implements CategoryGateway {
     }
 
     @Override
-    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
-        return null;
-    }
-
-    @Override
     public Pagination<Category> findAll(SearchQuery aQuery) {
-        return null;
+        final var page = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Sort.Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        // Busca dinamica pelo criterio terms (name ou description)
+        final var specifications = Optional.ofNullable(aQuery.terms())
+                .filter(str -> !str.isBlank())
+                .map(this::assembleSpecification)
+                .orElse(null);
+
+        final var pageResult =
+                this.categoryRepository.findAll(Specification.where(specifications), page);
+
+        return new Pagination<>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(CategoryEntity::toAggregate).toList()
+        );
+
     }
 
+//    @Override
+//    public Pagination<Category> findAll(SearchQuery aQuery) {
+//        return null;
+//    }
+
     @Override
-    public List<CategoryID> existsByIds(Iterable<CategoryID> ids) {
-        return null;
+    public List<CategoryID> existsByIds(final Iterable<CategoryID> categoryIDs) {
+        final var ids = StreamSupport.stream(categoryIDs.spliterator(), false)
+                .map(CategoryID::getValue)
+                .toList();
+        return this.categoryRepository.existsByIds(ids).stream()
+                .map(CategoryID::from)
+                .toList();
+    }
+
+    private Specification<CategoryEntity> assembleSpecification(final String str) {
+        final Specification<CategoryEntity> nameLike = SpecificationUtils.like("name", str);
+        final Specification<CategoryEntity> descriptionLike = SpecificationUtils.like("description", str);
+        return nameLike.or(descriptionLike);
     }
 }
